5669a365115cafb40d838790b5ec438ac8410205
ad4nd4 <93083137+ad4nd4@users.noreply.github.com>
2024-06-29 16:37:13 +0200

    Update Aunthenicator.ts

--- server/src/middlewares/Aunthenicator.ts
+++ server/src/middlewares/Aunthenicator.ts
@@ -1,13 +1,31 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
 
-class Aunthenicator {
+class Authenticator {
+    private static SECRET_KEY = 'your_secret_key';
 
     public static verifyToken(token: string): JwtPayload | null {
         try {
-            const payload = jwt.verify(token, Aunthenicator.SECRET_KEY) as JwtPayload;
+            const payload = jwt.verify(token, Authenticator.SECRET_KEY) as JwtPayload;
             return payload;
         } catch (error) {
             return null;
+        }
+    }
+    public static authenticate(req: Request, res: Response, next: NextFunction): void {
+        const authHeader = req.headers.authorization;
+
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
+            const payload = Authenticator.verifyToken(token);
+
+            if (payload) {
+                next();
+            } else {
+                res.status(401).json({ message: 'Invalid token' });
+            }
+        } else {
+            res.status(401).json({ message: 'Authorization header missing' });
         }
     }
 }
