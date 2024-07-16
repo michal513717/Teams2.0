e140f898d1cacd1f4a50aeb4e6b9c0936ba62e83
PFlak <piotr.andrzej.flak@gmail.com>
2024-07-17 01:21:26 +0200

    auth guard update

--- front/src/guards/AuthGuard.tsx
+++ front/src/guards/AuthGuard.tsx
@@ -1,23 +1,24 @@
-import { useUser } from "@/context/UserContext"
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useUser } from "@/context/UserContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-type Props = {component: React.FC}
+type Props = { component: React.FC };
 
-const AuthGuard: React.FC<Props> = ({component: Component}) => {
-    const {user} = useUser()
-    const navigate = useNavigate();
+const AuthGuard: React.FC<Props> = ({ component: Component }) => {
+  const { user, authCheck } = useUser();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (user) {
-            return;
+  useEffect(() => {
+    if (!user) {
+      authCheck().then((val) => {
+        if (val != 200) {
+          navigate("/login");
         }
-        navigate('/login')
-    }, [Component]);
+      });
+    }
+  }, [Component]);
 
-    return(
-        <Component></Component>
-    )
-}
+  return <Component></Component>;
+};
 
-export default AuthGuard
+export default AuthGuard;
