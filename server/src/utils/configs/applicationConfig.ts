bed326fbf0635ff36b05c99829959fdf08092df3
Michal <michal513717@gmail.com>
2024-07-23 21:03:15 +0200

    Small fix for cors

--- server/src/utils/configs/applicationConfig.ts
+++ server/src/utils/configs/applicationConfig.ts
@@ -4,8 +4,8 @@ export const APPLICATION_CONFIG = {
   DEBUG_REQUEST: true,
   APPLICATION_PORT: 8080,
   CORS_CONFIG: {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
+    origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:5174', 'http://localhost:5173'], //TMP "*" shouldn't be posted
+    credentials: true,
     optionSuccessStatus: 200
   }
 };
